0262ee6c49fa543c4fd79fc78b6846881bd0d003
Aurélien Strimelle <aurelien.strimelle@bstorm.be>
2024-02-09 15:26:31 +0100

    Demo Effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
+import { useCallback, useState } from 'react';
 import './App.css'
 import CounterCom from './containers/counter-com/counter-com';
 
 function App() {
 
+  const [isVisible, setVisible] = useState(false);
+
+  const handleVisibilityCounter = useCallback(() => {
+    setVisible(v => !v);
+  });
+
   return (
     <>
       <h1>Demo communication entre composant</h1>
-      <CounterCom />
+      <div>
+        <button onClick={handleVisibilityCounter}>
+          {isVisible ? 'Caché' : 'Affiché'} le compteur
+        </button>
+      </div>
+      {isVisible && (
+        <CounterCom />
+      )}
     </>
   )
 }
--- src/containers/counter-com/counter-com.jsx
+++ src/containers/counter-com/counter-com.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import DisplayCount from '../../components/display-count/display-count';
 import BtnActionCount from '../../components/btn-action-count/btn-action-count';
 
@@ -7,6 +7,21 @@ const INITIAL_COUNT = 0;
 const CounterCom = () => {
 
     const [countValue, setCountValue] = useState(INITIAL_COUNT);
+
+    useEffect(() => {
+        // Montage initial
+        const backupTitle = document.title;
+
+        return () => {
+            // Démontage final
+            document.title = backupTitle;
+        }
+    }, []);
+    
+    useEffect(() => {
+        // Montage initial + Toutes les updates de "countValue"
+        document.title = `Compteur → ${countValue}`;
+    }, [countValue]);
 
     const handleIncrDecr = useCallback((direction, step) => {
         setCountValue(cv => (direction === 'incr') ? cv + step : cv - step);
@@ -20,6 +35,7 @@ const CounterCom = () => {
         <>
             <DisplayCount count={countValue} />
             <BtnActionCount onIncrDecr={handleIncrDecr} />
+            <BtnActionCount onIncrDecr={handleIncrDecr} step={5} />
             {countValue !== INITIAL_COUNT && (
                 <button onClick={handleReset}>Reset</button>
             )}
